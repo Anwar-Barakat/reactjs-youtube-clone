7205cba08d8d3b25ae59859017103bf326272819
Anwar-Barakat <brkatanwar0@gmail.com>
2024-06-22 21:36:05 +0400

    finishing the youtube home page

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,6 +7,7 @@ import {
   demoVideoTitle,
   demoVideoUrl,
 } from "../utils/constants";
+import { CheckCircle } from "@mui/icons-material";
 
 const VideoCard = ({
   video: {
@@ -15,32 +16,39 @@ const VideoCard = ({
   },
 }) => {
   return (
-    <Card>
+    <Card
+      sx={{
+        width: { xs: "100%", sm: "358px", md: "340px" },
+        boxShadow: "none",
+        borderRadius: 0,
+      }}
+    >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
           sx={{ width: 350, height: 180 }}
         ></CardMedia>
-        <CardContent sx={{ background: "#1e1e1e", height: 106 }}>
-          <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
-            <Typography variant="subtitle1" fontWeight={"bold"} color={"#fff"}>
-              {snippet?.title?.slice(0, 60) || demoVideoTitle.slice(0, 60)}
-            </Typography>
-          </Link>
-          <Link
-            to={
-              snippet?.channelId
-                ? `/channel/${snippet?.channelId}`
-                : demoChannelUrl
-            }
-          >
-            <Typography variant="subtitle1" fontWeight={"bold"} color={"#fff"}>
-              {snippet?.channelTitle || demoChannelTitle}
-            </Typography>
-          </Link>
-        </CardContent>
       </Link>
+      <CardContent sx={{ background: "#1e1e1e", height: 106 }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight={"bold"} color={"#fff"}>
+            {snippet?.title?.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+          </Typography>
+        </Link>
+        <Link
+          to={
+            snippet?.channelId
+              ? `/channel/${snippet?.channelId}`
+              : demoChannelUrl
+          }
+        >
+          <Typography variant="subtitle2" color="gray" fontWeight={"bold"}>
+            {snippet?.channelTitle || demoChannelTitle}
+            <CheckCircle sx={{ fontSize: 12, color: "gray", ml: "5px" }} />
+          </Typography>
+        </Link>
+      </CardContent>
     </Card>
   );
 };
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -5,7 +5,13 @@ import { VideoCard, ChannelCard } from "./";
 const Videos = ({ videos }) => {
   console.log(videos);
   return (
-    <Stack direction={"row"} flexWrap={"wrap"}>
+    <Stack
+      direction={"row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
       {videos.map((item, index) => (
         <Box key={index}>
           {item.id.videoId && <VideoCard video={item}></VideoCard>}
